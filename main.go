258ad7365b1d0ab29ffefa23ed0737c0fc5696ba
Sabyasachi Patra <asabya@gmail.com>
2019-05-21 07:42:35 +0530

    more flags added

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"text/tabwriter"
 )
 
 var directory string
@@ -23,11 +24,20 @@ func main()  {
 	helpCommand := flag.NewFlagSet("help", flag.ExitOnError)
 
 	runCommand := flag.NewFlagSet("run", flag.ExitOnError)
+
 	livereload := runCommand.Bool("livereload", false, "run with livereload")
 	l := runCommand.Bool("l", false, "run with livereload")
 
 	generateCommand := flag.NewFlagSet("generate", flag.ExitOnError)
-	//logCommand := flag.NewFlagSet("log", flag.ExitOnError)
+
+	logCommand := flag.NewFlagSet("log", flag.ExitOnError)
+
+	clearLog := logCommand.Bool("clear", false, "Clear log file")
+	c := logCommand.Bool("c", false, "Clear log file")
+
+	dockerizeCommand := flag.NewFlagSet("dockerize", flag.ExitOnError)
+
+	buildCommand := flag.NewFlagSet("build", flag.ExitOnError)
 
 	switch os.Args[1] {
 		case "init", "i":
@@ -38,6 +48,12 @@ func main()  {
 			helpCommand.Parse(os.Args[2:])
 		case "generate", "g":
 			generateCommand.Parse(os.Args[2:])
+		case "log":
+			logCommand.Parse(os.Args[2:])
+		case "dockerize", "d":
+			dockerizeCommand.Parse(os.Args[2:])
+		case "build", "b":
+			buildCommand.Parse(os.Args[2:])
 		default:
 			fmt.Printf("%q is not valid command.\n", os.Args[1])
 			os.Exit(2)
@@ -45,7 +61,7 @@ func main()  {
 
 	if initCommand.Parsed() {
 
-		// Clone repository : clone letsGo
+		// Clone repository : clone letsgo
 		importPath = os.Args[2]
 		directory = os.Args[3]
 
@@ -59,7 +75,7 @@ func main()  {
 		}
 		path := strings.TrimSuffix(out.String(), "\n")+"/"+directory
 
-		fmt.Println("Cloning letsGo into : " + directory)
+		fmt.Println("Cloning letsgo into : " + directory)
 
 		_, err = git.PlainClone(path, false, &git.CloneOptions{
 			URL:      "https://github.com/letsgo-framework/letsGo",
@@ -71,13 +87,21 @@ func main()  {
 
 		fmt.Println("Cloning complete")
 
+		// Checkout latest tag
+		checkout := exec.Command("git", "checkout", "1.0.0-beta.0")
+		checkout.Dir = path
+		err = checkout.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		// Change package name : change package name in glide.yaml to your package name
 		read, err := ioutil.ReadFile(path+"/glide.yaml")
 		if err != nil {
 			panic(err)
 		}
 
-		newContents := strings.Replace(string(read), "letsGo", importPath+"/"+directory, -1)
+		newContents := strings.Replace(string(read), "github.com/letsgo-framework/letsgo", importPath+"/"+directory, -1)
 
 		err = ioutil.WriteFile(path+"/glide.yaml", []byte(newContents), 0)
 		if err != nil {
@@ -101,12 +125,15 @@ func main()  {
 			fmt.Println("graphql refracted")
 		}
 
+		/* No middleware available
 		err = filepath.Walk(path+"/middlewares", Visit)
 		if err != nil {
 			panic(err)
 		} else {
 			fmt.Println("middlewares refracted")
 		}
+		*/
+
 		err = filepath.Walk(path+"/routes", Visit)
 		if err != nil {
 			panic(err)
@@ -179,28 +206,86 @@ func main()  {
 	}
 
 	if runCommand.Parsed() {
-		runCommand.Parse(os.Args[2:])
-		if *livereload == true || *l == true{
-			_, _ = exec.Command("fresh").Output()
-		} else {
-			_, _ = exec.Command("go", "run", "main.go").Output()
-		}
+		if *livereload == true || *l == true {
+			freshCommand := exec.Command("fresh")
+			freshCommand.Stdout = os.Stdout
+			freshCommand.Stderr = os.Stderr
 
-		// TODO: Show output on console
+			_ = freshCommand.Run()
+		} else {
+			runMainCommand := exec.Command("go", "run", "main.go")
+			runMainCommand.Stdout = os.Stdout
+			runMainCommand.Stderr = os.Stderr
+
+			_ = runMainCommand.Run()
+		}
 	}
 
 	if helpCommand.Parsed() {
 		Usage()
 	}
+
+	if logCommand.Parsed() {
+		if *clearLog == true || *c == true{
+			emptyLogFile, err := os.Create("./log/letsgo.log")
+			if err != nil {
+				log.Fatal(err)
+			}
+			emptyLogFile.Close()
+
+			fmt.Println("Log cleared")
+		} else {
+			lineCount := os.Args[2]
+			tailCommand := exec.Command("tail", "-"+lineCount+"f", "log/letsgo.log")
+			tailCommand.Stdout = os.Stdout
+			tailCommand.Stderr = os.Stderr
+
+			_ = tailCommand.Run()
+		}
+	}
+
+	if dockerizeCommand.Parsed() {
+		binaryName := os.Args[2]
+		read, err := ioutil.ReadFile("./Dockerfile")
+		if err != nil {
+			panic(err)
+		}
+
+		newContents := strings.Replace(string(read), "letsgo", binaryName, -1)
+
+		err = ioutil.WriteFile("./Dockerfile", []byte(newContents), 0)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println("Dockerized")
+	}
+
+	if buildCommand.Parsed() {
+		build := exec.Command("go", "build")
+		build.Stdout = os.Stdout
+		build.Stderr = os.Stderr
+
+		_ = build.Run()
+
+		fmt.Println("Binary is ready")
+	}
 }
 
 func Usage() {
-	fmt.Fprintf(os.Stderr, "Usage of %s:\n\n", os.Args[0])
-	fmt.Fprintf(os.Stderr, "init <PROJECT_NAME> : Create a new letsgo project\n")
-	fmt.Fprintf(os.Stderr, "generate <FILE_TYPE> <FILE_NAME> : Generate file of controller of type\n")
-	fmt.Fprintf(os.Stderr, "run : Run your project\n")
-	fmt.Fprintf(os.Stderr, "\t -livereload or -l \t with livereload\n")
-	fmt.Fprintf(os.Stderr, "log <ACTION> : Tail or Clear log\n")
+	w := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
+	fmt.Fprintf(w, "Usage of %s:\n\n", os.Args[0])
+	fmt.Fprintf(w, "log <ACTION> \t Tail or Clear log\n")
+	fmt.Fprintf(w, "\t -clear or -c to clrear log\n")
+	fmt.Fprintf(w, "\t <LINE_NUMBER> to tail log file\n")
+	fmt.Fprintf(w, "init <IMPORT_PATH> <PROJECT_NAME> \t Create a new letsgo project \t short i\n")
+	fmt.Fprintf(w, "generate <FILE_TYPE> <FILE_NAME> \t Generate file of controller of type \t short g\n")
+	fmt.Fprintf(w, "build \t Build binary \t short b\n")
+	fmt.Fprintf(w, "dockerize <BINARY_NAME> \t Dockerize your project \t short d\n")
+	fmt.Fprintf(w, "help \t Print usage \t short h\n")
+	fmt.Fprintf(w, "run \t Run your project \t short r\n")
+	fmt.Fprintf(w, "\t -livereload or -l with livereload\n")
+	w.Flush()
 }
 
 
